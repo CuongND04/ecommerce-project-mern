41664458375cf8a2ad6c58b3b2c935e0d3ee427d
Nguyễn Đức Cường <uteu237@gmail.com>
2024-09-04 22:48:24 +0700

    ✅ filter and sort

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -20,12 +20,52 @@ const getOneProduct = asyncHandler(async (req, res) => {
   });
 });
 
+// Filtering, sorting & pagination
 const getListProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json({
-    success: products ? true : false,
-    productDatas: products ? products : "Cannot get list products",
-  });
+  // không gán trực tiếp vì muốn tạo một bản sao không ảnh hưởng đến bản gốc
+  const queries = { ...req.query };
+  // tách các trường đặc biệt ra khỏi query
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach((el) => delete queries[el]);
+
+  // format các operators đúng theo đúng cú pháp của mongoose
+  /*
+    // price[gt] : 3000 === price : { gt : 3000}
+    req.query { price: { gt: '5000' }, title: 'điều' }
+    formatedQueries { price: { '$gt': '5000' }, title: 'điều' }
+   */
+  let queryString = JSON.stringify(queries);
+  // thêm "$" trước các operator
+  queryString = queryString.replace(
+    /\b(gte|gt|lte|lt)\b/g,
+    (matchEl) => `$${matchEl}`
+  );
+  const formatedQueries = JSON.parse(queryString);
+
+  // Filtering
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  // chỗ này không thêm await vì chờ thêm sort, rồi xuống dưới then mới xử lí khi nó trả về response
+  let queryCommand = Product.find(formatedQueries);
+  // Sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    queryCommand = queryCommand.sort(sortBy);
+  }
+
+  // Excute Query
+  queryCommand
+    .then(async (response) => {
+      const counts = await Product.find(formatedQueries).countDocuments();
+      return res.status(200).json({
+        success: response ? true : false,
+        counts,
+        products: response ? response : "Cannot get products",
+      });
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 });
 
 const updateProduct = asyncHandler(async (req, res) => {
