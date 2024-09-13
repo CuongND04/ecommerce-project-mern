36c2bf89a4d86d3a34b3186e550ea13208274089
Nguyễn Đức Cường <uteu237@gmail.com>
2024-09-13 11:16:23 +0700

    ⚡️ call api get categories with redux toolkit

---
+++ client/src/store/asyncActions.js
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import * as apis from "../apis";
+
+// định nghĩa một cái action
+export const getCategories = createAsyncThunk(
+  "app/categories",
+  async (data, { rejectWithValue }) => {
+    const response = await apis.apiGetCategories();
+    if (!response.success) {
+      return rejectWithValue(response);
+    }
+    return response.data; // payload
+  }
+);
---
+++ client/src/ultils/helpers.js
@@ -0,0 +1,8 @@
+// chuyển tên tiếng việt thành slug không dấu
+export const createSlug = (string) =>
+  string
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split(" ")
+    .join("-");
