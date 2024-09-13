860e46b0df770d4f42b1482f7a7170d33e70192f
Nguyễn Đức Cường <uteu237@gmail.com>
2024-09-13 17:57:13 +0700

    ✨ new arrivals slider

--- client/src/components/BestSeller.js
+++ client/src/components/BestSeller.js
@@ -5,7 +5,6 @@ import Slider from "react-slick";
 const tabs = [
   { id: 1, name: "best sellers" },
   { id: 2, name: "new arrivals" },
-  { id: 3, name: "tablet" },
 ];
 var settings = {
   dots: false,
@@ -19,25 +18,34 @@ const BestSeller = () => {
   const [bestSellers, setBestSellers] = useState(null);
   const [newProducts, setNewProducts] = useState(null);
   const [activeTab, setActiveTab] = useState(1);
+  const [products, setProducts] = useState(null);
   const fetchProducts = async () => {
     // chờ 2 api trả về hết mới xử lý
     const response = await Promise.all([
       apiGetProducts({ sort: "-sold" }),
       apiGetProducts({ sort: "-createdAt" }),
     ]);
-    if (response[0]?.success) setBestSellers(response[0].products);
+    if (response[0]?.success) {
+      setBestSellers(response[0].products);
+      setProducts(response[0].products);
+    }
     if (response[1]?.success) setNewProducts(response[1].products);
   };
   useEffect(() => {
     fetchProducts();
   }, []);
+  useEffect(() => {
+    if (activeTab === 1) setProducts(bestSellers);
+    if (activeTab === 2) setProducts(newProducts);
+  }, [activeTab]);
+
   return (
     <>
-      <div className="flex text-[20px] gap-8 pb-4 border-b-2 border-main">
+      <div className="flex text-[20px]  ml-[-32px]">
         {tabs.map((el) => (
           <span
             key={el.id}
-            className={`font-semibold cursor-pointer capitalize text-gray-400 ${
+            className={`font-semibold px-8 border-r cursor-pointer capitalize text-gray-400 ${
               activeTab === el.id ? "text-gray-900" : ""
             }`}
             onClick={() => setActiveTab(el.id)}
@@ -46,10 +54,14 @@ const BestSeller = () => {
           </span>
         ))}
       </div>
-      <div className="mt-4 ">
+      <div className="mx-[-10px] border-t-2 border-main pt-4 ">
         <Slider {...settings}>
-          {bestSellers?.map((el) => (
-            <Product key={el.id} productData={el} />
+          {products?.map((el) => (
+            <Product
+              key={el.id}
+              productData={el}
+              isNew={activeTab === 1 ? false : true}
+            />
           ))}
         </Slider>
       </div>
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,12 +1,30 @@
 import React from "react";
-
-const Product = ({ productData }) => {
+import { formatMoney } from "../ultils/helpers";
+import newImg from "../assets/new.png";
+import trendingImg from "../assets/trending.png";
+const Product = ({ productData, isNew }) => {
   return (
-    <div className="w-1/3">
-      <img
-        src={productData?.images[0] || ""}
-        className="w-full h-[243px] object-cover"
-      />
+    <div className="w-full text-base px-[10px]">
+      <div className="w-full border p-[15px] flex flex-col items-center">
+        <div className="w-full relative">
+          <img
+            src={
+              productData?.thumb ||
+              "https://apollobattery.com.au/wp-content/uploads/2022/08/default-product-image.png"
+            }
+            className="w-[243px] h-[243px] object-cover"
+          />
+          <img
+            src={isNew ? newImg : trendingImg}
+            alt=""
+            className="absolute top-[0px] right-[0px] w-[100px] h-[35px] object-cover"
+          />
+        </div>
+        <div className="flex flex-col gap-1 mt-[15px] items-start w-full ">
+          <span className="line-clamp-1">{productData?.title}</span>
+          <span>{`${formatMoney(productData?.price)} VND`}</span>
+        </div>
+      </div>
     </div>
   );
 };
